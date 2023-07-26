87c8bc59eb5af91abdefee1a1429127460b4593d
bumjun <jbj3713@naver.com>
2023-07-26 13:03:09 +0900

    core/js 형 변환 학습

---
+++ 1.core-js/3.형변환/3.단축평가.js
@@ -0,0 +1,30 @@
+
+// 단축평가
+/*
+    T && T
+    T && T
+    F && T
+    F && F
+
+
+    T || T
+    T || f
+    f || T
+    f || f
+*/ 
+
+
+// or연산 : 첫번째 trythy를 반환
+
+console.log('hello' || 'world');
+console.log(0 || '안녕');
+
+// AND 연산 : 첫번째 falsy를 반환
+
+console.log('HELLO' && 'WORLD');
+console.log(null && '메롱');
+
+
+/*
+    
+*/
