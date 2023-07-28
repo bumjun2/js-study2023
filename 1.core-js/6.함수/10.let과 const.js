3d6f1d9e3ba7ada2449728a72197bfee453a941a
bumjun <jbj3713@naver.com>
2023-07-28 10:44:40 +0900

    스코프 완

---
+++ 1.core-js/6.함수/10.let과 const.js
@@ -0,0 +1,38 @@
+
+let x;
+x = 10;
+console.log(x);
+
+const y = 'hello';
+console.log(y);
+
+
+const COLOR_YELLOW = '#FF0';
+const COLOR_GREEN = '#0F0';
+const COLOR_BLACK = '#000';
+
+// $div.style.background=COLOR_YELLOW;
+// $span.style.background=COLOR_YELLOW;
+// $div.style.background=COLOR_GREEN;
+// $div.style.background=COLOR_BLACK; 
+
+
+// 객체는 const로 바꿔라 시발아
+const kim = {
+    name : '김철수',
+    age : 30
+};
+
+kim.name = '김마이클';
+
+// kim = {
+//     name:'박영희',
+//     age:40
+// };
+
+console.log(kim);
+
+
+
+let a =10;
+const b = 20;
