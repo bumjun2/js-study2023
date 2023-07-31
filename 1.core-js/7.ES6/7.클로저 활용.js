84e13561b8118966d01bb5694b16cca3bf48be02
bumjun <jbj3713@naver.com>
2023-07-31 10:30:47 +0900

    클로저 학습 완.

---
+++ 1.core-js/7.ES6/7.클로저 활용.js
@@ -0,0 +1,62 @@
+/*
+const increase = (() => {
+    let count = 0
+    return() => ++count;
+})();
+
+
+
+const dicrease = (() => {
+    let count = 0
+    return() => --count;
+})();
+
+
+
+console.log(increase);
+console.log(dicrease);
+console.log(dicrease);
+console.log(increase);
+console.log(increase);
+
+
+
+const countClosure = () => {
+    let count = 0;
+
+    const increase = () => ++count;
+    const dicrease = () => --count;
+
+    return{
+        increase,
+        dicrease
+    };
+};
+
+const counter = countClosure();
+
+console.log(counter.increase());
+console.log(counter.increase());
+console.log(counter.dicrease());
+console.log(counter.increase());
+console.log(counter.increase());
+*/
+
+const {increase, decrease} = (() =>{
+    let count = 0;
+    return {
+        increase : () => ++count,
+        decrease : () => --count
+    };
+})();
+
+console.log(increase());
+console.log(increase());
+console.log(decrease());
+console.log(increase());
+console.log(increase());
+console.log(increase());
+console.log(increase());
+
+
+
