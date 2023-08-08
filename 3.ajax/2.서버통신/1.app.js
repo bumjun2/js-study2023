896ed2e93177954f9e94872307cf341eee8178aa
bumjun2 <jbj3713@naver.com>
2023-08-08 09:47:11 +0900

    ajax : put, delete 요청 보내기 학습

--- 3.ajax/2.서버통신/1.app.js
+++ 3.ajax/2.서버통신/1.app.js
@@ -44,6 +44,8 @@ xhr.onload = e => {
     postList.forEach(post =>{
         const $postLi = document.createElement('li');
         $postLi.classList.add('post-item');
+        // li태그에 식별 아이디를 부여
+        $postLi.dataset.postId = post.id;
         $postLi.innerHTML =`
         <h2>${post.title}</h2>
         <p>${post.body}</p>
@@ -83,13 +85,45 @@ const fetchNewPost = (e) =>{
 
 $addForm.addEventListener('submit',fetchNewPost);
 
-document.getElementById('go-link').addEventListener('click',e => {
-    const flag = confirm('진짜 이동 할것인가 자네 ?');
-    if(!flag) {
-        console.log('넌 못간다 임마 ! ');
-        e.preventDefault(); //태그의 기본 기능을 없앰
-        //기본 기능: a -> 링크이동 기능
-        //          checkbox -> 체크기능
-        //          form -> 서버에 데이터를 주면서 새로고침
-    }
-});
+// 실제로 삭제요청을 보내는 함수
+const fetchDelete = (id) =>{
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', `http://localhost:8282/posts/${id}`)
+
+    xhr.send();
+
+    // 응답 처리
+    xhr.onload = () =>{
+        if(xhr.status === 200){
+            alert('삭제성공');
+        }else{
+            alert('삭제실패');
+        }
+    };
+};
+
+const deletePostHandler = e =>{
+    // if(e,target,querySelector(button) !== e.target)
+    if(!e.target.matches('button')) return;
+    console.log('삭제클릭');
+
+    // 삭제 클릭대상 아이디 잡아오기
+    console.log('삭제클릭');
+    const id = e.target.closest('.post-item').dataset.postId;
+
+    fetchDelete(id);
+};
+
+// 삭제 이벤트 등록
+$postUl.addEventListener('click', deletePostHandler);
+
+// document.getElementById('go-link').addEventListener('click',e => {
+//     const flag = confirm('진짜 이동 할것인가 자네 ?');
+//     if(!flag) {
+//         console.log('넌 못간다 임마 ! ');
+//         e.preventDefault(); //태그의 기본 기능을 없앰
+//         //기본 기능: a -> 링크이동 기능
+//         //          checkbox -> 체크기능
+//         //          form -> 서버에 데이터를 주면서 새로고침
+//     }
+// });
