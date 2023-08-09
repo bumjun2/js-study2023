dbfa2e85d178a0dd95991bcf68abc362f7124f3b
bumjun2 <jbj3713@naver.com>
2023-08-09 10:15:51 +0900

    ajax : 종합실습 일정관리프로그램 - 할일등록하기

--- 3.ajax/TodoList/assets/scripts/app_.js
+++ 3.ajax/TodoList/assets/scripts/app_.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:8282/todes'
+const URL = 'http://localhost:8585/todes'
 // step1. db.json에 있는 todos를 화면에 렌더링하기
 
 const $todoList = document.querySelector('.todo-list');
@@ -28,11 +28,45 @@ const renderTodos = (todoList) =>{
         const $checkbox = $newLi.querySelector('.checkbox input[type=checkbox]');
         // console.dir($checkbox);
         $checkbox.checked = done;
-        if(done) 
+        if(done) $checkbox.parentNode.classList.add('checked');
 
         $todoList.appendChild($newLi);
     });
 };
+
+// ========== 이벤트 영역 함수 ==============//
+const addTodpHandler = () =>{
+    // 1.클릭이벤트가 잘 일어나나?
+    // console.log('클릭');
+
+    // 2.클릭하면 일단 왼쪽에 인풋의 텍스트를 읽어야함
+    // 2-1 인풋부터 찾자
+    const $textInput = document.getElementById('todo-text');
+
+    // 2-2. 인풋안에 텍스트를 가져와
+    const inputText = $textInput.value;
+
+    // 3. 그럼 서버에 이 데이터를 보내서 저장하자
+    // -> fetch가 필요하다. 저장이나까 POST해야지
+    // -> payload를 API 스펙에 막제 만들어 보내야 함
+    const payload = {
+        text : inputText,
+        done : false
+    };
+
+    fetchTodos(URL, 'POST', payload)
+        .then(res =>{
+            if(res.status ===200 || res.status === 201){
+                console.log('등록 성공');
+            }else{
+                console.log('등록실패');
+            }
+        });
+};
+
+// step2. 할 일 등록 기능
+const $addBtn = document.getElementById('add')
+$addBtn.addEventListener('click', addTodpHandler);
 
 // ============ 앱 실행 =================//
 const init = () =>{
