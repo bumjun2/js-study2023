4936272fc205a1f20d0f9fe183be5281861561f8
bumjun2 <jbj3713@naver.com>
2023-08-10 21:09:06 +0900

    완.

--- 4.Module/event.js
+++ 4.Module/event.js
@@ -1,16 +1,15 @@
-import{ $p as $para } from './getDOM.js'
+import { $p as $para } from './getDOM.js';
 
-export const clickHandler = e =>{
-    $para.textContent = '하이';
+export const clickHandler = (e) => {
+  $para.textContent = '하이';
 };
 
-export default class{
-    constructor(name, age){         
-        this.name = names;
-        this.age = age;
-    }
-    info(){
-        alert(`${this.name} : 저는 ${this.age}살 입니다.`);
-    } 
+export default class {
+  constructor(name, age) {
+    this.name = names;
+    this.age = age;
+  }
+  info() {
+    alert(`${this.name} : 저는 ${this.age}살 입니다.`);
+  }
 }
-
